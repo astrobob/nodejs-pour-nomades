6cf9bc0fb59f97c102fc7739fff0e445e396dad8
admin <admin@iMac-de-admin-5.local>
2016-03-10 13:04:43 +0100

    jour4

--- jour-4/index.js
+++ jour-4/index.js
@@ -3,7 +3,23 @@
  */
 var async = require('async');
 var MongoClient= require('mongodb').MongoClient;
+var ObjectID = require("mongodb").ObjectID;
 
-MongoClient.connect('url', function (err, db) {
+var url = 'mongodb://localhost:27017/nekotest';
 
+MongoClient.connect(url, function (err, db) {
+	/*
+	db.collection('cf.roms')
+	.insertOne({
+		"titre" : "hello", 
+		"artiste": [{"name" : "john"}, {"name" : 'mister P', "pseudo": "Prince"}]
+	}, function(err, results) {
+		if (err) throw err;
+		console.log(results);
+	});
+	*/
+	var comp = ["56e15766b8bcff01588ce576", "56e15956b8bcff01588ce586"];
+	db.collection('users').find({ 'competences.id' : {$in : comp}}).toArray().then(function (results) {
+		console.log(results);
+	});
 });
